a80838cf9b6c8212bb9bad9ac59f27f0ebccf049
qnxy <1632009255@qq.com>
2023-12-06 16:41:04 +0800

    sse

--- src/main/java/com/example/sse/controller/SSEController.java
+++ src/main/java/com/example/sse/controller/SSEController.java
@@ -1,4 +1,26 @@
-package com.example.sse.controller;/**
+package com.example.sse.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
  * @author Qnxy
- */public class SSEController {
+ */
+@RestController
+@RequestMapping("/sse")
+public class SSEController {
+
+
+    @GetMapping(value = "/interval/{sec}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> interval(@PathVariable Integer sec) {
+        return Flux.interval(Duration.ofSeconds(sec));
+    }
+
+
 }
--- src/test/java/com/example/sse/SseDemoApplicationTests.java
+++ src/test/java/com/example/sse/SseDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ssedemo;
+package com.example.sse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
